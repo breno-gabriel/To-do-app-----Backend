be7d07bbdbaaaf952b83824940455d703139ce19
Breno <brenomelogabriel2019@gmail.com>
2024-10-22 22:04:11 -0300

    feat: adding the cascade delete for the user

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 
+@UsePipes(ValidationPipe)
 @UseGuards(AuthGuard)
 @Controller('task')
 export class TaskController {
