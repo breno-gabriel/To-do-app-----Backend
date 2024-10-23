596d8da70fcd3fc386a0e8a4af874e49c203a7b2
Breno <brenomelogabriel2019@gmail.com>
2024-10-22 21:44:04 -0300

    feat: adding authentication to the taks routes

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { PrismaModule } from 'src/database/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [TaskController],
   providers: [TaskService],
 })
